0ae64731b2a7e5801f07c30aca55dee7267ad5d3
Aryan Agarwal <aryankush025@gmail.com>
2024-03-21 00:48:27 +0530

    Interface added for struct

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type hindiBot struct{}
 
 func main() {
-	fmt.Println("Hello, World!")
+	eb := englishBot{}
+	hb := hindiBot{}
+
+	printGreeting(eb)
+	printGreeting(hb)
+}
+
+func printGreeting(b bot) {
+	println(b.getGreeting())
+}
+
+func (eb englishBot) getGreeting() string {
+	return "Hi there!"
+}
+
+func (hb hindiBot) getGreeting() string {
+	return "Namestey!"
 }
